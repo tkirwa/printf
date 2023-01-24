927278b08ff47a547d4b90f15494ea608c7ba7a9
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-24 10:05:21 -0800

    Updated

--- _printf.c
+++ _printf.c
@@ -1,41 +1,41 @@
 #include "main.h"
 #include <stdarg.h>
 
+/**
+ * _printf - function that produces output according to a format
+ * @format: character string containing zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ */
 int _printf(const char *format, ...)
 {
 va_list args;
-int i = 0, j = 0, len = 0;
-char *str;
+int len = 0;
 
 va_start(args, format);
+len = process_format(format, args);
+va_end(args);
+
+return len;
+}
+
+/**
+ * process_format - process the format string and print output
+ * @format: character string containing zero or more directives
+ * @args: variable argument list
+ *
+ * Return: the number of characters printed
+ */
+int process_format(const char *format, va_list args)
+{
+int i = 0, len = 0;
+
 while (format[i])
 {
 if (format[i] == '%')
 {
 i++;
-switch (format[i])
-{
-case 'c':
-putchar(va_arg(args, int));
-len++;
-break;
-case 's':
-str = va_arg(args, char*);
-while (str[j])
-{
-putchar(str[j]);
-j++;
-len++;
-}
-j = 0;
-break;
-case '%':
-putchar('%');
-len++;
-break;
-default:
-return (-1);
-}
+len += handle_conversion(format[i], args);
 }
 else
 {
@@ -44,6 +44,57 @@ len++;
 }
 i++;
 }
-va_end(args);
-return (len);
+
+return len;
+}
+
+/**
+* handle_conversion - handle the conversion specifier and print output
+* @conversion: the conversion specifier (c, s, or %)
+* @args: variable argument list
+*
+* Return: the number of characters printed
+*/
+int handle_conversion(char conversion, va_list args)
+{
+int len = 0;
+
+switch (conversion)
+{
+case 'c':
+putchar(va_arg(args, int));
+len++;
+break;
+case 's':
+len += print_string(va_arg(args, char*));
+break;
+case '%':
+putchar('%');
+len++;
+break;
+default:
+return (-1);
+}
+
+return len;
+}
+
+/**
+* print_string - print a string to stdout
+* @str: the string to print
+*
+* Return: the number of characters printed
+*/
+int print_string(char *str)
+{
+int i = 0, len = 0;
+
+while (str[i])
+{
+putchar(str[i]);
+i++;
+len++;
+}
+
+return len;
 }
