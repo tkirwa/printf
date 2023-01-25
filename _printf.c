04bf8c1c39d8dc4c4cda29118373a4a0a23a4c44
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-25 01:07:21 -0800

    Updated

--- _printf.c
+++ _printf.c
@@ -1,13 +1,12 @@
-#include "main.h"
 #include <stdarg.h>
+#include "main.h"
 
 /**
- * _printf - function that produces output according to a format
- * @format: character string containing zero or more directives
- *
- * Return: the number of characters printed (excluding...
- * the null byte used to end output to strings)
- */
+* _printf - function that produces output according to a format
+* @format: character string containing zero or more directives
+*
+* Return: the number of characters printed (excluding the null byte used to end output to strings)
+*/
 int _printf(const char *format, ...)
 {
 va_list args;
@@ -21,12 +20,12 @@ return (len);
 }
 
 /**
- * process_format - process the format string and print output
- * @format: character string containing zero or more directives
- * @args: variable argument list
- *
- * Return: the number of characters printed
- */
+* process_format - process the format string and print output
+* @format: character string containing zero or more directives
+* @args: variable argument list
+*
+* Return: the number of characters printed
+*/
 int process_format(const char *format, va_list args)
 {
 int i = 0, len = 0;
@@ -40,18 +39,17 @@ len += handle_conversion(format[i], args);
 }
 else
 {
-putchar(format[i]);
+_putchar(format[i]);
 len++;
 }
 i++;
 }
-
 return (len);
 }
 
 /**
-* handle_conversion - handle the conversion specifier and print output
-* @conversion: the conversion specifier (c, s, or %)
+* handle_conversion - handle the specific conversion specifier
+* @conversion: conversion specifier
 * @args: variable argument list
 *
 * Return: the number of characters printed
@@ -63,37 +61,49 @@ int len = 0;
 switch (conversion)
 {
 case 'c':
-putchar(va_arg(args, int));
+_putchar(va_arg(args, int));
 len++;
 break;
 case 's':
 len += print_string(va_arg(args, char*));
 break;
 case '%':
-putchar('%');
+_putchar('%');
 len++;
 break;
 default:
-return (-1);
+_putchar('%');
+_putchar(conversion);
+len += 2;
+break;
 }
 
 return (len);
 }
 
 /**
-* print_string - print a string to stdout
-* @str: the string to print
+* print_string - print a given string
+* @str: string to be printed
 *
 * Return: the number of characters printed
 */
 int print_string(char *str)
 {
-int i = 0, len = 0;
-
-while (str[i])
+int len = 0, i;
+if (!str)
 {
-putchar(str[i]);
-i++;
+_putchar('(');
+_putchar('n');
+_putchar('u');
+_putchar('l');
+_putchar('l');
+_putchar(')');
+return (6);
+}
+
+for (i = 0; str[i]; i++)
+{
+_putchar(str[i]);
 len++;
 }
 
--- main.h
+++ main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-int _putchar(char);
 int _printf(const char *format, ...);
 int process_format(const char *format, va_list args);
 int handle_conversion(char conversion, va_list args);
 int print_string(char *str);
+int _putchar(char c);
 
 #endif /* MAIN_H */
