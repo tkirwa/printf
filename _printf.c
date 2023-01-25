fde18e4be935f8cd7f0c359ed34dd2610432dfdc
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-25 01:26:11 -0800

    Updated

--- _printf.c
+++ _printf.c
@@ -5,7 +5,8 @@
 * _printf - function that produces output according to a format
 * @format: character string containing zero or more directives
 *
-* Return: the number of characters printed (excluding the null byte used to end output to strings)
+* Return: the number of characters printed (excluding...
+* the null byte used to end output to strings)
 */
 int _printf(const char *format, ...)
 {
