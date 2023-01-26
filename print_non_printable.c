b6b2d1f63ada122868b8ee6717e7d662e6c87bb5
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-01-26 19:11:14 +0300

    Updated

--- print_non_printable.c
+++ print_non_printable.c
@@ -3,28 +3,29 @@
 #include <unistd.h>
 
 /**
- * print_string_S - Prints a string, handling non-printable characters
- * @s: The string to print
- *
+ * print_non_printable_string - prints a string, with non-printable characters
+ * represented as \x, followed by the ASCII code value in hexadecimal
+ * (upper case - always 2 characters)
+ * @s: the string to print
  * Return: number of characters printed
  */
 int print_non_printable_string(char *s)
 {
-    int count = 0;
-    for (; *s; s++)
-    {
-        if (*s >= 32 && *s < 127)
-        {
-            putchar(*s);
-            count++;
-        }
-        else
-        {
-            putchar('\\');
-            putchar('x');
-            count += print_hex((unsigned char)*s, 1);
-            count += 2;
-        }
-    }
-    return count;
+int count = 0;
+for (; *s; s++)
+{
+if (*s >= 32 && *s < 127)
+{
+putchar(*s);
+count++;
+}
+else
+{
+putchar('\\');
+putchar('x');
+count += print_hex((unsigned char)*s, 1);
+count += 2;
+}
+}
+return (count);
 }
